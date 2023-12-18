630866446699e5ed96e0315fec227a682c7a15e9
Vlad Marian Panait <133703150+vladex11@users.noreply.github.com>
2023-12-18 10:45:13 +0200

    tema1_p2.py

--- tema1_p2.py
+++ tema1_p2.py
@@ -1,43 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-#se citesc datele din fisierul CSV
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')  # importarea datelor din fisierul data.csv
 
-# Se creaza o figura cu subplots
-fig, axs = plt.subplots(3, 1, figsize=(12, 18))
-
-# grafic pt primele 6 valori
 X = 6
-colors1 = plt.cm.viridis(np.linspace(0, 1, X))
-df_head = df.head(X)
-df_head.plot(x=df.columns[0], y=df.columns[1], kind='bar', ax=axs[0], color=colors1, edgecolor='black')
-axs[0].set_title(f'Primele {X} valori')
-axs[0].set_ylabel(df.columns[1])
-axs[0].set_xlabel(df.columns[0])
-axs[0].legend().set_visible(True)
-
-# Adăugăm etichete cu valori pentru fiecare bară în subplotul 1
-for i, val in enumerate(df_head[df.columns[1]]):
-    axs[0].text(i, val + 0.1, str(val), ha='center', va='bottom')
-
-# grafic 2: ultimele 10 valori
 Y = 10
-colors2 = plt.cm.inferno(np.linspace(0, 1, Y))
-df_tail = df[['Durata', 'Puls']].tail(Y)
-df_tail.plot(kind='bar', ax=axs[1], color=colors2, edgecolor='black')
-axs[1].set_title(f'Ultimele {Y} valori pentru Durata si Puls')
-axs[1].set_ylabel('Valoare')
-axs[1].set_xlabel('Index')
 
-# grafic pentru toate valorile
-colors3 = plt.cm.plasma(np.linspace(0, 1, len(df)))
-df.plot(x=df.columns[0], y=df.columns[1], kind='scatter', ax=axs[2], color=colors3, s=50, edgecolor='black', alpha=0.7)
-axs[2].set_title('Toate valorile')
-axs[2].set_ylabel(df.columns[1])
-axs[2].set_xlabel(df.columns[0])
+# in primul grafic se afiseaza toate valorile
+plt.figure(figsize=(15, 10))  # dimensionarea ferestrei
+plt.plot(df.index, df['Durata'], label='Durata', marker='X', color='blue') #plot() deseneaza liniile intre punctele date ale graficului
+plt.plot(df.index, df['Puls'], label='Puls', marker='X', color='red')
+plt.plot(df.index, df['MaxPuls'], label='MaxPuls', marker='X', color='grey')
+plt.plot(df.index, df['Calorii'], label='Calorii', marker='X', color='green')
+plt.title('toate valorile')  # denumirea graficului
+plt.legend()  # afisarea legendei graficului
+plt.show() #afisarea graficului
 
-# afisarea graficelor
-plt.tight_layout()
+# primele 6 valori pt toate valorile
+plt.figure(figsize=(15, 10))  # dimensionarea ferestrei unde se va afisa graficul
+plt.plot(df.index[:6], df['Durata'][:6], label='Durata', marker='X', color='blue')
+plt.plot(df.index[:6], df['Puls'][:6], label='Puls', marker='X', color='red')
+plt.plot(df.index[:6], df['MaxPuls'][:6], label='MaxPuls', marker='X', color='grey')
+plt.plot(df.index[:6], df['Calorii'][:6], label='Calorii', marker='X', color='green')
+plt.title('primele 6 valori')
+plt.legend()
+plt.show()
+
+# ultimele 10 valori pt puls si durata
+plt.figure(figsize=(15, 10))  # dimensionarea ferestrei unde se va afisa graficul
+plt.plot(df.index[-10:], df['Durata'].tail(10), label='Durata', marker='X', color='blue')
+plt.plot(df.index[-10:], df['Puls'].tail(10), label='Puls', marker='X', color='red')
+plt.title('ultimele 10 valori')
+plt.legend()
 plt.show()
